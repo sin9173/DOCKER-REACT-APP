b66f806ff126ce04d67ac8eafbd61fc5f347b1ee
sin9173 <sid9173@gmail.com>
2023-01-20 13:17:32 +0900

    Travis Test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          learn react2
         </a>
       </header>
     </div>
